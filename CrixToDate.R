2ee301ccde6db1efeb01e1153ca7ff30830be929
Raphael C. G. Reule <34136491+120BPM@users.noreply.github.com>
2017-12-20 20:56:02 +0100

    Update CrixToDate.R

--- CrixToDate.R
+++ CrixToDate.R
@@ -10,8 +10,9 @@ plot(crix, type = "l", col = "blue3", lwd = 3, xlab = "Date", ylab = "Performanc
 ###if you need different color or different axis name, you can change it in respective parameters
 
 
-# Graph 2 (CRIX components)
-# Volatility of logreturns of CRIX (monthly aggregated standard deviation)
+
+# Graph 2 (volatility)
+# volatility of logreturns of CRIX (monthly aggregated standard deviation)
 
 require(zoo)
 #getting returns
@@ -22,8 +23,8 @@ agg.days = aggregate(returns ~ MY, ret.table, length)
 agg.month$days = days$returns
 agg.month$MY = as.Date(agg.month$MY)
 colnames(agg.month) = c("date", "month.std", "days")
-agg.month$monthlyvola = sqrt(agg.month$days)*agg.month$month.std*100
+agg.month$monthlyvola=sqrt(agg.month$days)*agg.month$month.std*100
 
 plot(agg.month$date, agg.month$monthlyvola, type = "l", col = "grey",lwd = 3, xlab = "Date", 
 ylab = "Monthly aggregated returns volatility")
-axis(1,at = agg.month$date,labels = format(agg.month$date,"%Y-%m"),las = 1)
+axis(1, at = agg.month$date,labels=format(agg.month$date,"%Y-%m"),las = 1)
