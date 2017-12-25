1b6605bf5817deaa007e6b9914312200d4020127
Raphael C. G. Reule <34136491+120BPM@users.noreply.github.com>
2017-12-25 14:31:05 +0100

    Add files via upload

---
+++ CTD_CRIX_1d_24h/24h/CrixToDate_24h.R
@@ -0,0 +1,13 @@
+# Graph CRIX
+
+require(jsonlite)
+require(curl)
+json_file <- "http://crix.hu-berlin.de/data/crix_hf.json"
+crix <- fromJSON(json_file)
+
+
+crix$date <- as.POSIXct(crix$date, format="%Y-%m-%d  %H:%M:%S")
+
+plot(crix$date, crix$price/1000, xaxt='n', pch=20, las=1, 
+     xlab='24th to 25th of December 2017', ylab='Price/1000')
+axis.POSIXct(1, crix$date, format="%H:%M:%S")
