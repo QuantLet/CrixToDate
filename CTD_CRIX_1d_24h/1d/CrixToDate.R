365e624dc9ed684de28d7ebdb9c5c0a745cfead7
Raphael C. G. Reule <34136491+120BPM@users.noreply.github.com>
2017-12-28 21:15:53 +0100

    Update CrixToDate.R

--- CTD_CRIX_1d_24h/1d/CrixToDate.R
+++ CTD_CRIX_1d_24h/1d/CrixToDate.R
@@ -1,10 +1,15 @@
 # Graph CRIX
 
 require(jsonlite)
-require(curl)
-json_file <- "http://crix.hu-berlin.de/data/crix.json"
-crix <- fromJSON(json_file)
-crix$date <- as.Date(crix$date)
-plot(crix, type = "l", col = "blue3", lwd = 3, xlab = "Date", ylab = "Performance of CRIX")
+require(zoo)
+crix <- read.csv("crixdata.csv", header=TRUE, sep=";")
+crix$date = as.Date(crix$date)
+plot(crix, type = "l", col = "blue3", lwd = 2, xlab = "Date", ylab = "Performance of CRIX", cex.lab = 2)
 
-###if you need different color or different axis name, you can change it in respective parameters
+
+##require(jsonlite)
+##require(curl)
+##json_file <- "http://crix.hu-berlin.de/data/crix.json"
+##crix <- fromJSON(json_file)
+##crix$date <- as.Date(crix$date)
+##plot(crix, type = "l", col = "blue3", lwd = 3, xlab = "Date", ylab = "Performance of CRIX")
