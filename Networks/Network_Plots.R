34a44fb02956f7e962778961e3065c8825ca17ef
Raphael C. G. Reule <34136491+120BPM@users.noreply.github.com>
2017-12-28 19:20:31 +0100

    Update Network_Plots.R

--- Networks/Network_Plots.R
+++ Networks/Network_Plots.R
@@ -2,13 +2,13 @@ install.packages("igraph")
 library(igraph) 
 
 #centralized
-tr <- make_tree(40, children = 3, mode = "undirected")
+tr = make_tree(40, children = 3, mode = "undirected")
 plot(tr, vertex.size=10, vertex.label=NA)
 
 #decentralized
-tr <- make_tree(40, children = 4, mode = "undirected")
+tr = make_tree(40, children = 4, mode = "undirected")
 plot(tr, vertex.size=10, vertex.label=NA)
 
 #distributed
-zach <- graph("Zachary")
+zach = graph("Zachary")
 plot(zach, vertex.size=10, vertex.label=NA)
