72d8f42450443b123373722b77778fc041af873a
Raphael C. G. Reule <34136491+120BPM@users.noreply.github.com>
2017-12-28 21:26:50 +0100

    Update CRIX_Volatility.R

--- CTD_CRIX_Volatility/CRIX_Volatility.R
+++ CTD_CRIX_Volatility/CRIX_Volatility.R
@@ -21,6 +21,6 @@ agg.month$date=as.Date(agg.month$date)
 colnames(agg.month)=c("date", "month.std", "days")
 agg.month$monthlyvola=sqrt(agg.month$days)*agg.month$month.std*100
 
-plot(agg.month$date, agg.month$monthlyvola, type = "l", col = "grey",lwd = 2, xlab = "Date",
+plot(agg.month$date, agg.month$monthlyvola, type = "l", col = "red",lwd = 2, xlab = "Date",
      ylab = "Monthly aggregated returns volatility", cex.lab = 1.5)
 axis(1,at=agg.month$date,labels=format(agg.month$date,"%Y-%m"),las=1)
