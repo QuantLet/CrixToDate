5bb3aec0afdea35abde267067a4cd6031b8fb93b
Raphael C. G. Reule <34136491+120BPM@users.noreply.github.com>
2017-12-28 21:25:05 +0100

    Update CRIX_Volatility.R

--- CTD_CRIX_Volatility/CRIX_Volatility.R
+++ CTD_CRIX_Volatility/CRIX_Volatility.R
@@ -1,11 +1,18 @@
 require(jsonlite)
-require(curl)
 require(zoo)
-json_file <- "http://crix.hu-berlin.de/data/crix.json"
-crix <- fromJSON(json_file)
-crix$date <- as.Date(crix$date)
+
+##require(jsonlite)
+##require(curl)
+##require(zoo)
+##json_file <- "http://crix.hu-berlin.de/data/crix.json"
+##crix <- fromJSON(json_file)
+
+crix <- read.csv("crixdata.csv", header=TRUE, sep=";")
+crix$date = as.Date(crix$date)
+
 
 #volatility of logreturns (monthly aggregated standard deviation)
+
 returns= data.frame(date = as.yearmon(crix$date[-1], "%y-%m"), returns = diff(log(crix$price)))
 agg.month= aggregate(returns ~ date, returns, sd)
 agg.days = aggregate(returns ~ date, returns, length)
@@ -14,6 +21,6 @@ agg.month$date=as.Date(agg.month$date)
 colnames(agg.month)=c("date", "month.std", "days")
 agg.month$monthlyvola=sqrt(agg.month$days)*agg.month$month.std*100
 
-plot(agg.month$date, agg.month$monthlyvola, type = "l", col = "red",lwd = 3, xlab = "Date", 
-     ylab = "Monthly aggregated returns volatility")
+plot(agg.month$date, agg.month$monthlyvola, type = "l", col = "grey",lwd = 2, xlab = "Date",
+     ylab = "Monthly aggregated returns volatility", cex.lab = 1.5)
 axis(1,at=agg.month$date,labels=format(agg.month$date,"%Y-%m"),las=1)
