fa5ce9c85915f9573b2bebef29b661c28b5ab82a
Raphael C. G. Reule <34136491+120BPM@users.noreply.github.com>
2018-01-01 18:16:39 +0100

    Add files via upload

---
+++ CTD_CRIX_Full_24h/Full/CrixToDate.R
@@ -0,0 +1,21 @@
+# Graph CRIX
+
+require(jsonlite)
+require(curl)
+json_file <- "http://crix.hu-berlin.de/data/crix.json"
+crix <- fromJSON(json_file)
+crix$date <- as.Date(crix$date)
+plot(crix, type = "l", col = "blue3", lwd = 3, xlab = "Date", ylab = "Performance of CRIX", cex.lab = 2)
+
+
+
+
+### Alternative
+# Graph CRIX
+
+require(jsonlite)
+require(zoo)
+crix <- read.csv("crixdata.csv", header=TRUE, sep=";")
+crix$date = as.Date(crix$date)
+plot(crix, type = "l", col = "blue3", lwd = 2, xlab = "Date", ylab = "Performance of CRIX", cex.lab = 2)
+
