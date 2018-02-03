e9e595ebba47083dd20464880c0872115d3c81ac
Raphael C. G. Reule <34136491+120BPM@users.noreply.github.com>
2018-02-03 14:39:55 +0100

    Update CrixToDate.R

--- CTD_CRIX_Full_24h/Full/CrixToDate.R
+++ CTD_CRIX_Full_24h/Full/CrixToDate.R
@@ -1,7 +1,11 @@
 # Graph CRIX
 
-require(jsonlite)
-require(curl)
+# install and load packages
+libraries = c("curl", "jsonlite", "zoo")
+lapply(libraries, function(x) if (!(x %in% installed.packages())) {
+  install.packages(x)
+})
+       
 json_file <- "http://crix.hu-berlin.de/data/crix.json"
 crix <- fromJSON(json_file)
 crix$date <- as.Date(crix$date)
@@ -13,8 +17,12 @@ plot(crix, type = "l", col = "blue3", lwd = 3, xlab = "Date", ylab = "Performanc
 ### Alternative
 # Graph CRIX
 
-require(jsonlite)
-require(zoo)
+# install and load packages
+libraries = c("curl", "jsonlite", "zoo")
+lapply(libraries, function(x) if (!(x %in% installed.packages())) {
+  install.packages(x)
+})
+
 crix <- read.csv("crixdata.csv", header=TRUE, sep=";")
 crix$date = as.Date(crix$date)
 plot(crix, type = "l", col = "blue3", lwd = 2, xlab = "Date", ylab = "Performance of CRIX", cex.lab = 2)
