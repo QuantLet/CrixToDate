2b1f11244563619351dbb41164916526e3ef6987
Raphael C. G. Reule <34136491+120BPM@users.noreply.github.com>
2018-12-13 13:33:09 +0100

    Update
    Update

--- CTD_CRIX_Full_24h/Full/CrixToDate.R
+++ CTD_CRIX_Full_24h/Full/CrixToDate.R
@@ -1,31 +1,54 @@
-# Graph CRIX from CRIX.berlin (website)
+# General File Preperation
 
-# install and load packages
+# Reset Workspace
+rm(list = ls(all = TRUE))
+
+# Load Necessary Packages
 libraries = c("jsonlite", "curl", "zoo")
-lapply(libraries, function(x) if (!(x %in% installed.packages())) {
-  install.packages(x)
-})
+lapply(libraries, function(x)
+  if (!(x %in% installed.packages())) {
+    install.packages(x)
+  }
+)
 lapply(libraries, library, quietly = TRUE, character.only = TRUE)
-       
-json_file <- "http://crix.hu-berlin.de/data/crix.json"
+
+#-------------------------------------------------------------------------------
+
+# Graph CRIX - based on website
+
+# Get Data
+json_file <- "http://data.thecrix.de/data/crix.json"
 crix <- fromJSON(json_file)
 crix$date <- as.Date(crix$date)
-plot(crix, type = "l", col = "blue3", lwd = 3, xlab = "Date", ylab = "Performance of CRIX", cex.lab = 2)
+
+# Regular Crix
+plot(crix, type = "l", col = "steelblue", lwd = 3, xlab = "Date", ylab = "", cex.lab = 1.5, xaxt = "n")
+axis.Date(1,at = seq(min(crix$date),max(crix$date),by = "6 mon"), format = "%m-%Y")
+title(ylab = "Performance of CRIX", line = 2, cex.lab = 2)
+
+# Log(Crix)
+plot(crix$date, log(crix$price), type = "l", col = "steelblue", lwd = 2, xlab = "Date", ylab = "", cex.lab = 2, xaxt = "n")
+axis.Date(1,at = seq(min(crix$date),max(crix$date),by = "6 mon"), format = "%m-%Y")
+title(ylab = "Performance of log(CRIX)", line = 2, cex.lab = 2)
 
 
 
 
-################################ Alternative
-# Graph CRIX from CRIXdata.csv (file)
+#-------------------------------------------------------------------------------
 
-# install and load packages
-libraries = c("curl", "zoo")
-lapply(libraries, function(x) if (!(x %in% installed.packages())) {
-  install.packages(x)
-})
-lapply(libraries, library, quietly = TRUE, character.only = TRUE)
+### Alternative
+# Graph CRIX - based on stored csv (not up-to-date)
 
+# Get Data
 crix <- read.csv("crixdata.csv", header=TRUE, sep=";")
 crix$date = as.Date(crix$date)
-plot(crix, type = "l", col = "blue3", lwd = 2, xlab = "Date", ylab = "Performance of CRIX", cex.lab = 2)
 
+# Regular Crix
+plot(crix, type = "l", col = "steelblue", lwd = 2, xlab = "Date", ylab = "", cex.lab = 2, xaxt = "n")
+axis.Date(1,at = seq(min(crix$date),max(crix$date),by = "6 mon"), format = "%m-%Y")
+title(ylab = "Performance of CRIX", line = 2, cex.lab = 2)
+
+# Log Log(Crix)
+plot(crix$date, log(crix$price), type = "l", col = "steelblue", lwd = 2, xlab = "Date", ylab = "", cex.lab = 2, xaxt = "n")
+axis.Date(1,at = seq(min(crix$date),max(crix$date),by = "6 mon"), format = "%m-%Y")
+title(ylab = "Performance of log(CRIX)", line = 2, cex.lab = 2)
